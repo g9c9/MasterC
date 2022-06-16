5e94ba41d06add269cb62999263301941ae6acd0
George <g9c9@outlook.com>
2022-06-15 23:09:48 -0700

    Array of structures challenge

---
+++ arrayofstructures.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+struct date {
+        int month;
+        int day;
+        int year;
+};
+
+struct person {
+        char name[32];
+        struct date tookoffice;
+};
+
+void printPresidents(struct person*);
+
+int main()
+{  
+    struct person presidents[4] = {
+        {"George Washington", {4, 30, 1789}},
+        {"Thomas Jefferson", {3, 4, 1801}},
+        {"Abraham Lincoln", {3, 4, 1861}},
+        {"Theodore Roosevelt", {9, 14, 1901}}
+    };
+
+    printPresidents(presidents);
+
+    struct person temp = presidents[1];
+    presidents[1] = presidents[2];
+    presidents[2] = temp;
+    printf("Swapping...done!");
+
+    printPresidents(presidents);
+    
+}
+
+void printPresidents(struct person* presidents)
+{
+    printf("Presidents:\n");
+    for(int i = 0; i < 4; i++)
+    {
+        printf("President %s took office on %d/%d/%d\n", 
+                presidents[i].name, 
+                presidents[i].tookoffice.month,
+                presidents[i].tookoffice.day,
+                presidents[i].tookoffice.year);
+    }
+}
